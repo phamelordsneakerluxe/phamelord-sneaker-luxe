659a8aa81088dc28c35c6799e1b4c26768e11c83
Phamelord Sneaker House <@gyasibenedict2@gmail.com>
2025-08-08 22:58:44 +0000

    Bind app to 0.0.0.0 and use dynamic port for Render

--- app.py
+++ app.py
@@ -73,4 +73,5 @@ def contact():
     return render_template('contact.html', sneaker_name=sneaker_name, phone=phone, email=email)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
